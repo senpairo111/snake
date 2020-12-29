daf991f449b6e04513e1cc5efbef6f7fd6706b6f
senpairo111 <74233627+senpairo111@users.noreply.github.com>
2020-12-29 17:39:19 +0200

    Add files via upload

--- snake game/apple.py
+++ snake game/apple.py
@@ -1,15 +1,17 @@
 import random
 from snake import Snake
-s = Snake()
 from snake import field
+s = Snake()
 class Apple:
     def __init__(self):
-        self.x = random.randint(0, field)
-        self.y = random.randint(0, field)
+        self.x = random.randint(-1, field)
+        self.y = random.randint(-1, field)
         self.score = 0
 
 
     def eaten(self):
-        self.x = random.randint(0, field)
-        self.y = random.randint(0, field)
+        self.x = random.randint(-1, field)
+        self.y = random.randint(-1, field)
         self.score += 1
+        s.snake_length += 1
+
